25f99449c3b1da9d32e81cf9d4bb2b6a0b773ba2
War-on <101604756+War-on@users.noreply.github.com>
2025-12-09 22:52:04 -0300

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,3 +1,1 @@
-document.evaluate('//*[@id="region-main"]/div/div/div/h2', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent='';
-document.querySelector('.no-link').remove();
-
+alert('XSS')
